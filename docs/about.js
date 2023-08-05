71b48b60ae8cdbceefa96e776a67e52662225fe7
xavierdpt <37592677+xavierdpt@users.noreply.github.com>
2023-08-05 22:17:11 +0200

    .

--- docs/about.js
+++ docs/about.js
@@ -1,13 +1,13 @@
 /* vim: set ts=2 sw=2 et number nowrap: */
 pages.about=prnt=>{
-	const text = txt => {
     /*
+	const text = txt => {
 		const p = xce('p');
 		xac(p,xctn(txt));
 		xac(topdiv,p);
-    */
 	};
 	text('This project is an attempt of seeing what can be created in a rather constrained environment.');
 	text('The IDE is Vim in Termux on an Android phone.');
 	text('Hosting is GitHub pages, so no server side.');
+    */
 };
