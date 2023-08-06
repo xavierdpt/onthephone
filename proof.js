ce6459e3f55189870cd8c9753e733d33525870af
xavierdpt <37592677+xavierdpt@users.noreply.github.com>
2023-08-06 10:58:08 +0200

    .

--- proof.js
+++ proof.js
@@ -4,6 +4,9 @@ pages.proof=prnt=>{
   const addDomain = domain => {
     variables[domain]={};
   }
+  const addVariable = (domain,variable) => {
+    variables[domain][variable] = true;
+  };
 	const newProof = xce('button');
   xac(newProof,xctn('New Proof'));
   xon(newProof,'click',()=>{
@@ -43,6 +46,49 @@ pages.proof=prnt=>{
 	const newVar = xce('button');
   xac(newVar,xctn('New Variable'));
   xon(newVar,'click',()=>{
+    const domainLabel = xce('label');
+    xac(domainLabel,xctn('Domain'));
+    xac(prnt,domainLabel );
+    const domainSelect = xce('select');
+    for(domain in variables) {
+      const option = xce('option');
+      option.name=domain;
+      xac(option,xctn(domain));
+      xac(domainSelect ,option);
+    }
+    xac(prnt,domainSelect );
+    const label = xce('label');
+    xac(label,xctn('Variable name'));
+    xac(prnt,label);
+    const input = xce('input');
+    input.type='text';
+    xac(prnt,input);
+    const save = xce('button');
+    xac(save,xctn('Save'));
+    xon(save,'click',()=>{
+      const domain = domainSelect.value;
+      const variable = input.value;
+      addVariable(domain,variable);
+      xrm(domainLabel);
+      xrm(domainSelect);
+      xrm(label);
+      xrm(input);
+      xrm(save);
+      xrm(cancel);
+    });
+    xac(prnt,save);
+    const cancel = xce('button');
+    xac(cancel,xctn('Cancel'));
+    xon(cancel,'click',()=>{
+      xrm(domainLabel);
+      xrm(domainSelect);
+      xrm(label);
+      xrm(input);
+      xrm(save);
+      xrm(cancel);
+    });
+    xac(prnt,cancel);
+  });
   });
   xac(prnt,newVar);
 	const newDef = xce('button');
